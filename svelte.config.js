8f9828f4e595252cab67270ccc6632967e118ca2
Matt Lundin <matt.lundin@outlook.com>
2022-09-05 09:33:43 -0500

    changed adapter-vercel to adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
 
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 const config = {
