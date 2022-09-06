8f6b092490a69fd32bbfdccda0ff827665f08471
Matt Lundin <matt.lundin@outlook.com>
2022-09-05 20:47:53 -0500

    adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
 
-import vercel from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 const config = {
@@ -10,17 +10,21 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
 	}
-	// kit: {
-	// 	adapter: adapter({
-	// 		fallback: '200.html',
-	// 		pages: 'build',
-	// 		assets: 'build',
-	// 		precompress: false
-	// 	}),
-	// 	prerender: { entries: [] }
-	// }
 };
 
 export default config;
