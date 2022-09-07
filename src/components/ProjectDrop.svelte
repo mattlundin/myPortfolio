74de5e080b4026502f33d75ebe34510f53a0ea5c
Matt Lundin <matt.lundin@outlook.com>
2022-09-07 13:17:51 -0500

    ProjectDrop margin on mobile

--- src/components/ProjectDrop.svelte
+++ src/components/ProjectDrop.svelte
@@ -51,30 +51,34 @@
 	});
 </script>
 
-<div
-	class="lg:w-11/12 lg:mx-auto h-full w-full max-w-screen-2xl bg-opacBlack my-12 border-2 border-lightBlue overflow-hidden"
->
-	<div class="fade-down text-5xl text-center pt-8 text-offWhite">{projectName}</div>
-	<div class="flex lg:flex-row md:p-12 md:gap-12 justify-between flex-col h-full p-4">
-		<img
-			src={projectImg}
-			alt={projectImgAlt}
-			class="fade-right h-auto max-h-[480px] lg:w-1/2 w-100"
-		/>
-		<div class="fade-left text-offWhite lg:w-1/2">
-			{projectDescription}
+<div class="flex items-center">
+	<div
+		class="lg:w-11/12 lg:mx-auto h-full w-full max-w-screen-2xl bg-opacBlack my-12 mx-2 border-2 border-lightBlue rounded-md overflow-hidden"
+	>
+		<div class="fade-down text-5xl text-center pt-8 text-offWhite">{projectName}</div>
+		<div class="flex lg:flex-row md:p-12 md:gap-12 justify-between flex-col h-full p-4">
+			<img
+				src={projectImg}
+				alt={projectImgAlt}
+				class="fade-right h-auto max-h-[480px] lg:w-1/2 w-100"
+			/>
+			<div class="md:mt-0 mt-8 fade-left text-offWhite lg:w-1/2">
+				{projectDescription}
+			</div>
 		</div>
-	</div>
-	<div class="fade-up bottom-8 flex items-center justify-around pb-12 w-full text-xl text-offWhite">
-		<a
-			href={projectURL}
-			class="text-2xl underline hover:text-lightBlue hover:scale-125 transition ease duration-300"
-			target="_blank">Site</a
+		<div
+			class="fade-up bottom-8 flex items-center justify-around pb-12 w-full text-xl text-offWhite"
 		>
-		<a
-			href={projectGitHub}
-			class="text-2xl underline hover:text-lightBlue hover:scale-125 transition ease duration-300"
-			target="_blank">Github</a
-		>
+			<a
+				href={projectURL}
+				class="text-2xl underline hover:text-lightBlue hover:scale-125 transition ease duration-300"
+				target="_blank">Site</a
+			>
+			<a
+				href={projectGitHub}
+				class="text-2xl underline hover:text-lightBlue hover:scale-125 transition ease duration-300"
+				target="_blank">Github</a
+			>
+		</div>
 	</div>
 </div>
