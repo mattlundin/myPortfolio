d06af69438a8a28a7638f9f9def7444111f5360b
Matt Lundin <matt.lundin@outlook.com>
2022-09-20 10:21:12 -0500

    updated QuintsArt URL

--- src/routes/Projects/QuintsArt/+page.svelte
+++ src/routes/Projects/QuintsArt/+page.svelte
@@ -10,6 +10,6 @@
 	projectImg="/images/QuintsArt.webp"
 	projectImgAlt="Quints Art"
 	projectDescription="This site was built (is being built) with SvelteKit, Tailwind, and GSAP. This is still a work in progress as I am currently waiting for the content, image files, logo, etc... What I have enjoyed most about this one so far is the incorporation of GSAP along with some new parallax ideas I have been playing with. The final result may look a little different but I think this is off to a good start."
-	projectURL="https://quints-art-1-6w7601n8p-mattlundin.vercel.app/"
+	projectURL="https://quints-art-1-he19nez9k-mattlundin.vercel.app/"
 	projectGitHub="https://github.com/mattlundin/quints-art-1"
 />
